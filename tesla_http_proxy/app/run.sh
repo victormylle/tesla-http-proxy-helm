c193dabb7dbee1b6661b3c8493c61235c085f4ed
Victor Mylle <victor.mylle@hotmail.com>
2024-05-01 02:47:58 +0200

    Small update

--- tesla_http_proxy/app/run.sh
+++ tesla_http_proxy/app/run.sh
@@ -1,16 +1,6 @@
 #!/bin/ash
 set -e
 
-# read options
-cp -n /app/config.sh /data
-. /data/config.sh
-
-# Exit if options not setup
-if [ $OPTIONS_COMPLETE != 1 ]; then
-  echo "Configuration options not set in /data/config.sh, exiting"
-  exit 0
-fi
-
 echo "Configuration Options are:"
 echo CLIENT_ID=$CLIENT_ID
 echo "CLIENT_SECRET=Not Shown"
